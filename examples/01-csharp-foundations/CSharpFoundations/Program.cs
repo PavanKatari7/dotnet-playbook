f86dab19ea9c6acb56557a4f61d335ebb9896f5c
PavanKatari7 <pavanyadav11497@gmail.com>
2026-09-07 07:24:25 +0530

    New changes to basic code

--- examples/01-csharp-foundations/CSharpFoundations/Program.cs
+++ examples/01-csharp-foundations/CSharpFoundations/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, Pavan!");
+
+Console.Write("What is your name? ");
+string? name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}! Welcome back to .net");
+
+Console.WriteLine("Hello Pavan");
